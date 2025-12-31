a2dffe808ca68457d114d95201d3f57a12dcfb3d
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-12-31 18:39:41 +0000

    Improve bash rendering

--- web/src/components/tool-renderers/bash-renderer.tsx
+++ web/src/components/tool-renderers/bash-renderer.tsx
@@ -56,8 +56,7 @@ export function BashRenderer(props: BashRendererProps) {
         </div>
         <div className="p-3 overflow-x-auto">
           <div className="flex items-start gap-2">
-            <span className="text-green-500 select-none">$</span>
-            <pre className="text-xs font-mono text-zinc-200 whitespace-pre-wrap break-all">
+            <pre className="text-xs font-mono m-0 p-0 bg-transparent text-zinc-200 whitespace-pre-wrap break-all">
               {command}
             </pre>
           </div>
